9e5c8c7d493787421c3c4540b39812045cd4fa53
bloodofme <awesome.dragon.wizard@gmail.com>
2021-05-28 16:28:42 +0800

    empty

--- Frontend/src/Login/Login.js
+++ Frontend/src/Login/Login.js
@@ -55,12 +55,12 @@ handleRoute = () =>{
                 >
 
                 <Form.Item
-                    label="Username"
-                    name="username"
+                    label="Email"
+                    name="email"
                     rules={[
                         {
                             required: true,
-                            message: 'Please input your username!',
+                            message: 'Please input your email!',
                         },
                     ]}
                     style={{minWidth: 500}}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,6 +7,7 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// Add request
 router.route('/add').post((req, res) => {
   const email = req.body.email;
   const firstName = req.body.firstName;
@@ -22,6 +23,29 @@ router.route('/add').post((req, res) => {
 
   newUser.save()
     .then(() => res.json('User Added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Find one slot by user's email
+router.route('/:email').get((req, res) => {
+  User.find({email: req.params.email})
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Update user details by the user's email
+router.route('/update/:email').post((req, res) => {
+  Slot.find({email: req.params.email})
+    .then(user => {
+      user.email = req.body.email;
+      user.firstName = req.body.firstName;
+      user.lastName = req.body.lastName;
+      user.password = req.body.password;
+
+      user.save()
+        .then(() => res.json('User Updated!'))
+        .catch(err =>   s.status(400).json('Error: ' + err));
+    })
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
