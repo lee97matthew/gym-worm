5bc7dfdd440fdd802299177f88e86296885b33f3
bloodofme <awesome.dragon.wizard@gmail.com>
2021-06-26 00:23:34 +0800

    updated redirects

--- Frontend/src/Home/Home.js
+++ Frontend/src/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout, Row, Col, Space } from 'antd';
 import 'antd/dist/antd.css';
 import './Home.css';
@@ -12,59 +12,85 @@ import history from "../history";
 const { Header, Content } = Layout;
 document.body.style = 'background: #74828F;';
 
-class Home extends Component {
-    constructor(props) {
-        super(props);
+function Home() {
 
-        this.state = {
-            currentUser: AuthService.getCurrentUser()
-        };
-    }
+    const currentUser = AuthService.getCurrentUser()
 
     if (currentUser) {
         AuthService.updateCurrentUser(currentUser.email, currentUser.password);
-    }   
-
-    render() {
-        const { currentUser } = this.state;
-        if (!currentUser) {
-            history.push('/');
-            //window.location.reload();
+    }     
+   
+    useEffect(() => {
+        if (AuthService.getCurrentUser() === null) {
+            history.push("/")
+            window.location.reload(false);
         }
+    }, [])
+
+    function Test() {
+        return (
+            <Layout>
+                <Header className='theTitle'>
+                    <p1>Hi {currentUser.firstName + " " + currentUser.lastName}</p1>
+                </Header>
+                <Content style={{ background: "#74828F" }}>
+                    <Layout className='layout'>
+                        <Row>
+                            <Col span={12}>
+                                <Row align='center'>
+                                    <Space direction="vertical" size={10} align='center'>
+                                        <p1 className='textHome'>Your Bookings</p1>
+                                        <DisplayBookings />
+                                    </Space>
+                                </Row>
+                            </Col>
+                            <Col span={12}>
+                                <Row align='center'>
+                                    <Space direction="vertical" size={10} align='center'>
+                                        <p1 className='textHome'>Your Credit</p1>
+                                        <Credits></Credits>
+                                    </Space>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Layout>
+                </Content>
+            </Layout>
+        )
+    }
 
         return (
             <div>
                 <Navbar />
                 <Layout>
-                    <Header className='theTitle'>
-                        <p1>Hi {currentUser.firstName + " " + currentUser.lastName}</p1>
-                    </Header>
-                    <Content style={{ background: "#74828F" }}>
-                        <Layout className='layout'>
-                            <Row>
-                                <Col span={12}>
-                                    <Row align='center'>
-                                        <Space direction="vertical" size={10} align='center'>
-                                            <p1 className='textHome'>Your Bookings</p1>
-                                            <DisplayBookings />
-                                        </Space>
-                                    </Row>
-                                </Col>
-                                <Col span={12}>
-                                    <Row align='center'>
-                                        <Space direction="vertical" size={10} align='center'>
-                                            <p1 className='textHome'>Your Credit</p1>
-                                            <Credits></Credits>
-                                        </Space>
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </Layout>
-                    </Content>
-                </Layout>
+                <Header className='theTitle'>
+                    <p1>Hi {currentUser.firstName + " " + currentUser.lastName}</p1>
+                </Header>
+                <Content style={{ background: "#74828F" }}>
+                    <Layout className='layout'>
+                        <Row>
+                            <Col span={12}>
+                                <Row align='center'>
+                                    <Space direction="vertical" size={10} align='center'>
+                                        <p1 className='textHome'>Your Bookings</p1>
+                                        <DisplayBookings />
+                                    </Space>
+                                </Row>
+                            </Col>
+                            <Col span={12}>
+                                <Row align='center'>
+                                    <Space direction="vertical" size={10} align='center'>
+                                        <p1 className='textHome'>Your Credit</p1>
+                                        <Credits></Credits>
+                                    </Space>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Layout>
+                </Content>
+            </Layout>
             </div>
         )
-    }
 }
 
 export default Home;
--- Frontend/src/Routes.js
+++ Frontend/src/Routes.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 //import "bootstrap/dist/css/bootstrap.min.css";
 
-//import AuthService from "./services/auth.service";
+import AuthService from "./services/auth.service";
 
 import Home from "./Home/Home";
 import Bookings from "./Bookings/Bookings";
@@ -40,18 +40,17 @@ class Routes extends Component {
         AuthService.logout();
     }
     */
-
     render() {
         //const { currentUser, showAdminBoard } = this.state;
         return (
             <BrowserRouter history={history}>
                 <Switch>
                     <Route path="/" exact component={Login} />
-                    <Route path="/Home" component={Home} />
-                    <Route path="/Bookings" component={Bookings} />
-                    <Route path="/Profile" component={Profile} />
+                    <Route path="/Home" component={AuthService.getCurrentUser() === null ? Login : Home} />
+                    <Route path="/Bookings" component={AuthService.getCurrentUser() === null ? Login : Bookings} />
+                    <Route path="/Profile" component={AuthService.getCurrentUser() === null ? Login : Profile} />
                     <Route path="/Signup" component={Signup} />
-                    <Route path="/MakeBookings" component={MakeBookings} />
+                    <Route path="/MakeBookings" component={AuthService.getCurrentUser() === null ? Login : MakeBookings} />
                 </Switch>
             </BrowserRouter>
         )
