bb22102fdd6f97b95a07ee9326328668725f2512
Matthew <e0556151@u.nus.edu>
2021-06-27 04:14:23 +0800

    Deploy Fix 03

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ require('./routes/slot.routes')(app);
 })*/
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './frontend/build/index.html'));
