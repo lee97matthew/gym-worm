2d36ce82b21307aec366cd7915b8a5901491de42
Matthew <e0556151@u.nus.edu>
2021-06-27 11:40:40 +0800

    Deploy Test 05

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -15,44 +15,49 @@ const API_URL = "https://gym-worm.herokuapp.com/api/slot/";
 document.body.style = 'background: #74828F;';
 
 function Home() {
+    const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
     const arrSlots = [];
     const [container, setContainer] = useState(null);
     const [slots, setSlots] = useState([])
-    
+
     const currentUser = AuthService.getCurrentUser()
 
     if (currentUser) {
         AuthService.updateCurrentUser(currentUser.email, currentUser.password);
-    }     
-   
+    }
+
     useEffect(() => {
-        if (AuthService.getCurrentUser() === null) {
-            history.push("/");
-            window.location.reload(false);
-        }
+        sleep(3000).then(() => {
+            if (AuthService.getCurrentUser() === null) {
+                history.push("/");
+                window.location.reload(false);
+            }
+        });
+
     }, [])
 
     useEffect(() => {
         const temp = []
-             currentUser.bookings.forEach(slot => {
-                 (async () => {
-                     const res = await axios.post(API_URL + 'retrieveSlot', { bookingID: slot });
-                     const posts = res.data.slot;
-                     temp.push([posts, slot])
-                     if (temp.length === currentUser.bookings.length) {
-                         setSlots(temp)
-                     }
-                     console.log(temp)
-                 })()
-             })
-         
-        }, []) 
-    
+        currentUser.bookings.forEach(slot => {
+            (async () => {
+                const res = await axios.post(API_URL + 'retrieveSlot', { bookingID: slot });
+                const posts = res.data.slot;
+                temp.push([posts, slot])
+                if (temp.length === currentUser.bookings.length) {
+                    setSlots(temp)
+                }
+                console.log(temp)
+            })()
+        })
+
+    }, [])
+
     function DisplayBookings(props) {
-        const isChecked = useRef([false, props.slot.date.slice(0,10), props.slot.startTime]);
+        const isChecked = useRef([false, props.slot.date.slice(0, 10), props.slot.startTime]);
 
         const onChange = (e) => {
-            isChecked.current = [e.target.checked, props.slot.date.slice(0,10), props.slot.startTime];
+            isChecked.current = [e.target.checked, props.slot.date.slice(0, 10), props.slot.startTime];
             console.log(isChecked);
             if (isChecked.current[0]) {
                 arrSlots.push(props.slot)
@@ -63,29 +68,29 @@ function Home() {
             }
             console.log(arrSlots)
         }
-    
+
         const Time = (time) => {
             return time <= 12 ? `${time}am` : `${time - 12}pm`
         }
-        
-        return(
+
+        return (
             <div>
                 <Card className='card'>
                     <Row gutter={10}>
-                    <Col style={{ padding: '8px 0' }} wrap="false">
-                        <p className='text'>{`Date: ${props.slot.date.slice(0,10)} Time: ${Time(props.slot.startTime)}`}</p>
-                    </Col>
+                        <Col style={{ padding: '8px 0' }} wrap="false">
+                            <p className='text'>{`Date: ${props.slot.date.slice(0, 10)} Time: ${Time(props.slot.startTime)}`}</p>
+                        </Col>
                     </Row>
                 </Card>
-                    
+
             </div>
         );
     }
 
-        return (
-            <div>
-                <Navbar />
-                <Layout>
+    return (
+        <div>
+            <Navbar />
+            <Layout>
                 <Header className='theTitle'>
                     <p1>Hi {currentUser.firstName + " " + currentUser.lastName}</p1>
                 </Header>
@@ -97,15 +102,15 @@ function Home() {
                                     <Space direction="vertical" size={10} align='center'>
                                         <p1 className='textHome'>Your Bookings</p1>
                                         <Breadcrumb target={() => container} id="slots" >
-                                            <Space 
-                                                style={{ background: "74828F", alignItems: "center" }} 
-                                                direction="vertical" size={'small'} 
+                                            <Space
+                                                style={{ background: "74828F", alignItems: "center" }}
+                                                direction="vertical" size={'small'}
                                                 align='center'
                                             >
-                                                { slots.length === 0 ? null : slots.forEach( element =>arrSlots.push(<DisplayBookings slot={element[0]}/>)) }
-                                                { slots.length === 0 ? <p1 className='textHome'> No Bookings </p1> : arrSlots.map(elements => <div> {elements} </div> ) }
+                                                {slots.length === 0 ? null : slots.forEach(element => arrSlots.push(<DisplayBookings slot={element[0]} />))}
+                                                {slots.length === 0 ? <p1 className='textHome'> No Bookings </p1> : arrSlots.map(elements => <div> {elements} </div>)}
                                             </Space>
-                                         </Breadcrumb>
+                                        </Breadcrumb>
                                     </Space>
                                 </Row>
                             </Col>
@@ -121,8 +126,8 @@ function Home() {
                     </Layout>
                 </Content>
             </Layout>
-            </div>
-        )
+        </div>
+    )
 }
 
 export default Home;
